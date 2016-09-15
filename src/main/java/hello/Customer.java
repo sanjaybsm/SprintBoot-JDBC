b3d6714616cd125be6230b2ef9d5f4fb24de1598
unknown <Sanjay_Shivanna@MYSHEZ94928L.ad.infosys.com>
2016-09-15 21:37:56 +0200

    Initial commit

---
+++ src/main/java/hello/Customer.java
@@ -0,0 +1,25 @@
+package hello;
+
+/**
+ * Created by Sanjay_Shivanna on 9/15/2016.
+ */
+public class Customer {
+    private long id;
+    private String firstName, lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    // getters & setters omitted for brevity
+
+}
